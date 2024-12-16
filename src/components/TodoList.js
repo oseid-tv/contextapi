80c3631181849fdf707c5b0e55f81a5977a15e15
Super Dev <superdev1031@gmail.com>
2024-12-16 09:38:35 -0500

    update Navbar according to the themeJ

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div className="ui list">
+      <p className="item">Plan the family trip</p>
+      <p className="item">Go for shopping for dinner</p>
+      <p className="item">Go for a walk</p>
+    </div>
+  );
+};
+
+export default TodoList;
