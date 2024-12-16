657f04be56e61b4a5df793797fabef910b5f1b6f
Super Dev <superdev1031@gmail.com>
2024-12-16 09:50:45 -0500

    update TodoList component based on the context

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-const TodoList = () => {
-  return (
-    <div className="ui list">
-      <p className="item">Plan the family trip</p>
-      <p className="item">Go for shopping for dinner</p>
-      <p className="item">Go for a walk</p>
-    </div>
-  );
-};
+class TodoList extends React.Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isDarkTheme, lightTheme, darkTheme } = this.context;
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return (
+      <div
+        style={{
+          background: theme.background,
+          color: theme.text,
+          textAlign: "center",
+          heigth: "140px",
+        }}
+      >
+        <p className="item">Plan the family trip</p>
+        <p className="item">Go for shopping for dinner</p>
+        <p className="item">Go for a walk</p>
+      </div>
+    );
+  }
+}
 
 export default TodoList;
